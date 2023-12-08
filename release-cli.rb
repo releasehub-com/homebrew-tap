10dc2731e2f01dcb5605b07e4601ed972d5c812e
goreleaserbot <goreleaser@carlosbecker.com>
2023-12-08 12:25:07 -0500

    Brew formula update for release version v0.28.1-beta

--- release-cli.rb
+++ release-cli.rb
@@ -5,11 +5,11 @@
 class ReleaseCli < Formula
   desc "CLI for Release (release.com)"
   homepage "https://release.com/"
-  version "0.28.0-beta"
+  version "0.28.1-beta"
 
   on_macos do
-    url "https://release-cli.s3.us-west-2.amazonaws.com/v0.28.0-beta/release_0.28.0-beta_Darwin_x86_64.tar.gz"
-    sha256 "68189b1718e6ca2a78652050b1dbca7c13d1f791313b813bf6f3eedba7bcce4a"
+    url "https://release-cli.s3.us-west-2.amazonaws.com/v0.28.1-beta/release_0.28.1-beta_Darwin_x86_64.tar.gz"
+    sha256 "96ef53de73907f4e66dbdb9707c43ea78f313613c5f13f0f9c1eddda20eb4d24"
 
     def install
       bin.install "release"
@@ -39,9 +39,9 @@ class ReleaseCli < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://release-cli.s3.us-west-2.amazonaws.com/v0.28.0-beta/release_0.28.0-beta_Linux_arm64.tar.gz"
-      sha256 "a1153f60dad363a5db3b53a4690289f873215fd8599ef822e9649b6f580815cc"
+    if Hardware::CPU.intel?
+      url "https://release-cli.s3.us-west-2.amazonaws.com/v0.28.1-beta/release_0.28.1-beta_Linux_x86_64.tar.gz"
+      sha256 "70aed9b0dc391ba7fa2efc486640d5ea6d2c97ebdbafadbd65896140a34d2f4c"
 
       def install
         bin.install "release"
@@ -59,9 +59,9 @@ class ReleaseCli < Formula
         (fish_completion/"release.fish").write output
       end
     end
-    if Hardware::CPU.intel?
-      url "https://release-cli.s3.us-west-2.amazonaws.com/v0.28.0-beta/release_0.28.0-beta_Linux_x86_64.tar.gz"
-      sha256 "8478dc480233294453fc5a6122f55d6f2045554cf3334ed2b1aa3bef92419fd7"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://release-cli.s3.us-west-2.amazonaws.com/v0.28.1-beta/release_0.28.1-beta_Linux_arm64.tar.gz"
+      sha256 "2abd6de4a17aacd2243e8367350dc4e38a7360e3bcb408974a8f87c7c989acb7"
 
       def install
         bin.install "release"
